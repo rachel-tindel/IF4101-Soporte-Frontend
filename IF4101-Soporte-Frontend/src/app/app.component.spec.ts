d3b1e2d3649f68a2295b4a333ede7300ba2acffa
Luis Carlos Aguilar Morales <luiscarlos.aguilar@ucr.ac.cr>
2021-07-22 09:39:49 -0600

    Update

--- IF4101-Soporte-Frontend/src/app/app.component.spec.ts
+++ IF4101-Soporte-Frontend/src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('IF4101-Soporte-Frontend');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('IF4101-Soporte-Frontend app is running!');
-  });
 });
